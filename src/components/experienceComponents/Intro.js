7a1656d771f3b97c5076c913375186afa4c21f3b
joycelynmng <jmng399@gmail.com>
2022-10-03 22:29:51 -0700

    fixed spacing resume

--- src/components/experienceComponents/Intro.js
+++ src/components/experienceComponents/Intro.js
@@ -1,8 +1,9 @@
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { LeftSection } from '../../styles/Intro.styled';
+import moreStyles from '../../styles/additionalStyles.module.css'
 
 const Intro = (props) => (
-  <Section row id="experience">
+  <Section row id="experience" className={moreStyles.backgroundColor}>
       <LeftSection>
           <SectionTitle main center>
               Experiences
